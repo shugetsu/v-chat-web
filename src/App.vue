2143e8d058e388f1afe36b9b50fa40e4d942193f
shugetsu <1968304304@qq.com>
2021-07-27 13:57:15 +0800

    chore: 制定代码风格规范

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,20 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+  import { defineComponent } from 'vue'
 
-export default defineComponent({
-  name: 'App'
-});
+  export default defineComponent({
+    name: 'App'
+  })
 </script>
 
 <style>
   #app {
     margin-top: 60px;
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
+    -webkit-font-smoothing: antialiased;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -moz-osx-font-smoothing: grayscale;
     text-align: center;
   }
 </style>
