77820dc6e95abb39b7918bf475f3df7460cd7d60
shugetsu <1968304304@qq.com>
2021-08-13 14:47:02 +0800

    feat(components): 添加 ConfigProvider 组件

---
+++ src/components/ConfigProvider/index.ts
@@ -0,0 +1,1 @@
+export { default as VConfigProvider } from './src/ConfigProvider.vue'
---
+++ src/components/ConfigProvider/src/ConfigProvider.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  import { NConfigProvider, NGlobalStyle } from 'naive-ui'
+</script>
+
+<template>
+  <NConfigProvider abstract>
+    <slot></slot>
+    <NGlobalStyle />
+  </NConfigProvider>
+</template>
+
+<style lang="scss"></style>
