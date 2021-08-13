2f64abc169219c898a642b9c438f1bb26cb4e37b
shugetsu <1968304304@qq.com>
2021-08-13 10:25:26 +0800

    feat(utils): 添加 ensureCssUnit 确保是 css 单位助手

---
+++ src/utils/helpers/ensureCssUnit.ts
@@ -0,0 +1,12 @@
+import { isNumber } from '../tools/is'
+
+/**
+ * @description 确保是 css 单位
+ * @param {string | number} value
+ * @param {string} unit
+ * @return {string}
+ */
+export function ensureCssUnit(value: string | number, unit = 'px'): string {
+  const cssUnit = isNumber(value) ? `${value}${unit}` : value
+  return cssUnit
+}
