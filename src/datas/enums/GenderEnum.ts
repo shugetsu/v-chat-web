29ecfee444472ad0eb60a4120899406fa4daf6a6
shugetsu <1968304304@qq.com>
2021-09-12 18:04:33 +0800

    feat(datas): 添加 enums/GenderEnum 性别类型

---
+++ src/datas/enums/GenderEnum.ts
@@ -0,0 +1,21 @@
+/**
+ * @date 2021-09-12 15:35:43
+ * @lastEditTime 2021-09-12 15:38:22
+ * @description 性别类型
+ * @filePath /src/datas/enums/GenderEnum.ts
+ */
+
+export const enum GenderEnum {
+  /**
+   * 中性
+   */
+  NEUTRAL = -1,
+  /**
+   * 女
+   */
+  WOMAN = 0,
+  /**
+   * 男
+   */
+  MALE = 1
+}
