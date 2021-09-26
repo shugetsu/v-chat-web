f151db1aec64213a28893680957d21f17a8c7bf4
shugetsu <1968304304@qq.com>
2021-09-26 14:51:03 +0800

    feat(components): 添加 Container/NoDataContainer 组件

---
+++ src/components/Container/index.ts
@@ -0,0 +1,1 @@
+export { default as VNoDataContainer } from './src/NoDataContainer.vue'
---
+++ src/components/Container/src/NoDataContainer.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import { NEmpty } from 'naive-ui'
+
+  const props = withDefaults(
+    defineProps<{
+      /**
+       * 是否无数据
+       * @default false
+       */
+      noData?: boolean
+    }>(),
+    {
+      noData: false
+    }
+  )
+</script>
+
+<template>
+  <div class="flex flex-col w-full overflow-hidden">
+    <div v-if="!props.noData" class="flex-1 overflow-hidden">
+      <slot></slot>
+    </div>
+
+    <slot v-else name="empty">
+      <NEmpty class="flex-1 justify-center" />
+    </slot>
+  </div>
+</template>
+
+<style lang="scss"></style>
