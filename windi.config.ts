847fb93643975e21b805f5a04e7650dc73183628
shugetsu <1968304304@qq.com>
2021-09-26 19:23:05 +0800

    chore:  修改 windi.config 配置

--- windi.config.ts
+++ windi.config.ts
@@ -1,6 +1,6 @@
 /**
  * @date 2021-07-28 14:38:13
- * @lastEditTime 2021-07-28 14:38:13
+ * @lastEditTime 2021-09-18 14:17:42
  * @description windicss 配置
  * @filePath /windi.config.ts
  * @see https://windicss.org/guide/configuration.html
@@ -12,5 +12,12 @@ export default defineConfig({
   attributify: {
     prefix: 'v:'
   },
-  darkMode: 'class'
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary-color)'
+      }
+    }
+  }
 })
